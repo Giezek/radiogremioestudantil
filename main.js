4f9ebf84985608868a3f2b98f9ea532bdf38a599
Giezek <104865473+Giezek@users.noreply.github.com>
2023-10-06 00:08:36 +0000

    grilo2

--- main.js
+++ main.js
@@ -3,6 +3,6 @@ document.querySelector("#som_tecla_risada").play();
 }
 document.querySelector(".tecla_risada").onclick = tocaSomRisada;
 function tocaSomGrilo(){
-    document.querySelector("#som_tecla_risada").play();
+    document.querySelector("#som_tecla_grilo").play();
     }
     document.querySelector(".tecla_grilo").onclick = tocaSomGrilo;
