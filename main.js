847e59830a2276d6d450e5c9b7852499c8a9ca40
Giezek <104865473+Giezek@users.noreply.github.com>
2023-10-06 00:06:33 +0000

    erro

--- main.js
+++ main.js
@@ -1,4 +1,4 @@
 function tocaSomRisada(){
 document.querySelector("#som_tecla_risada").play();
 }
-document.querySelector("tecla_risada").onclick = tocaSomRisada;
+document.querySelector(".tecla_risada").onclick = tocaSomRisada;
