bcad5a351a5378220d6b65091731f555b5c1f12a
Giezek <104865473+Giezek@users.noreply.github.com>
2023-10-06 00:14:32 +0000

    som

--- main.js
+++ main.js
@@ -6,3 +6,7 @@ function tocaSomGrilo(){
     document.querySelector("#som_tecla_grilo").play();
     }
     document.querySelector(".tecla_grilo").onclick = tocaSomGrilo;
+function tocaSomErrado(){
+    document.querySelector("#som_tecla_errado").play();
+    }
+    document.querySelector(".tecla_errado").onclick = tocaSomErrado;
